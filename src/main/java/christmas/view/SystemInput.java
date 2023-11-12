9c71f51b05b9347b887498968cfe698683e6962e
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-12 20:56:56 +0900

    feat(Christmas): get menu and amount from user;

--- src/main/java/christmas/view/SystemInput.java
+++ src/main/java/christmas/view/SystemInput.java
@@ -8,4 +8,10 @@ public class SystemInput {
         String date = Console.readLine();
         return Integer.parseInt(date);
     }
+
+    public static String readOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려주세요. (e.g., 해산물파스타-2,레드와인-1,초코케이크-1): ");
+        String menuPrice = Console.readLine();
+        return menuPrice;
+    }
 }
