aeca1b47539e757d2ddaa2e687410d99d2559217
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-13 11:51:51 +0900

    feat(Christmas): build code that determines if the day is weekend or week
    - use Calendar class to determine if the day is weekend or not

--- src/main/java/christmas/domain/DateUtil.java
+++ src/main/java/christmas/domain/DateUtil.java
@@ -1,3 +1,19 @@
 package christmas.domain;
+
+import java.util.Calendar;
 public class DateUtil {
+    public static boolean isWeekday(int visitDate) {
+        if (visitDate >= 1 && visitDate <= 30) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.DAY_OF_MONTH, visitDate);
+            int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+            return dayOfWeek != Calendar.SATURDAY && dayOfWeek != Calendar.SUNDAY;
+        } else {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public static boolean isWeekend(int visitDate) {
+        return !isWeekday(visitDate);
+    }
 }
