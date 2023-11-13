9cf4f3bc28be53780aeb32d35c3b9c231494073b
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-13 18:29:47 +0900

    feat(Christmas): make code in controller to make sure discount events implements only when price exceeds 10000

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -7,21 +7,24 @@ import christmas.view.SystemInput;
 import christmas.domain.Menu;
 
 public class ChristmasController {
-    //날짜를 입력받는다
-    //메뉴 및 수량을 입력받느다
-    // 입력받은 날짜 및 수량을 기준으로 할인율을 계산한다
-    // 주문한 메뉴를 출력한다
-    // 할인전 총 주문금액을 출력한다
-    // 증정 메뉴 여부를 출력한다
-    // 증정 이벤트에 해당하지 않는 경우, 증정 메뉴 "없음"으로 보여 주세요.
-    // 혜택 여부를 출력한다
-    // 할인 후 예상 결제 금액 출력한다
-    // 12월 달 예상 금액을 출력한다
+
     public static void run() {
         int visitDate = SystemInput.readDate();
-
         User user = new User();
         String orderMenuPrice = SystemInput.readOrder();
+        if (checkRunGame(orderMenuPrice)) {
+            processOrderInput(orderMenuPrice, user);
+            Calculator.calculateDiscount(user);
+            if (calculateChampagne(user)) {
+                user.setTotalBenefit(user.getTotalDiscount() + Discount.CHAMPAGNE_DISCOUNT.getValue());
+            } else {
+                user.setTotalBenefit(user.getTotalDiscount());
+            }
+        } else {
+            System.out.println("[ERROR] 주문 금액이 너무 낮습니다. 최소 주문 금액은 10,000원입니다.");
+        }
+    }
+    public static void processOrderInput(String orderMenuPrice, User user) {
         String[] items = orderMenuPrice.split(",");
         for (String item : items) {
             String[] parts = item.split("-");
@@ -29,25 +32,23 @@ public class ChristmasController {
             int quantity = Integer.parseInt(parts[1]);
             user.addToOrder(menu, quantity);
         }
-        Calculator.calculateDiscount(user);
-        if (calculateChampagne(user)) {
-            user.setTotalBenefit(user.getTotalDiscount() + Discount.CHAMPAGNE_DISCOUNT.getValue());
-        } else {
-            user.setTotalBenefit(user.getTotalDiscount());
-        }
     }
 
-        public static Menu getMenuByName (String name){
-            for (Menu menu : Menu.values()) {
-                if (menu.getName().equalsIgnoreCase(name)) {
-                    return menu;
-                }
+    public static Menu getMenuByName(String name) {
+        for (Menu menu : Menu.values()) {
+            if (menu.getName().equalsIgnoreCase(name)) {
+                return menu;
             }
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
-
-        public static boolean calculateChampagne (User user){
-            return user.getTotalDiscount() >= 12_000;
-        }
+        throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
+
+    public static boolean calculateChampagne(User user) {
+        return user.getTotalDiscount() >= 12_000;
+    }
+
+    private static boolean checkRunGame(String totalOrderAmount) {
+        return Integer.parseInt(totalOrderAmount) >= 10_000;
+    }
+}
 
