aa5f9c811ce449fce44fb5c732bbc2ba97de24c4
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-15 00:12:18 +0900

    fix(Christmas): make sure if there are no benefits 없음 is print

--- src/main/java/christmas/view/SystemQuantityOutput.java
+++ src/main/java/christmas/view/SystemQuantityOutput.java
@@ -23,20 +23,40 @@ public class SystemQuantityOutput {
         System.out.println("\n<할인 전 총주문 금액>");
         System.out.println(decimalFormat.format(user.getTotalOrderAmount()) + "원");
     }
+    public static void printGift(User user) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###");
+        Discount champagneDiscount = Discount.CHAMPAGNE_DISCOUNT;
+        String formattedDiscount = "없음";
+        System.out.println("\n<증정 메뉴>");
+        if (Calculator.calculateChampagne(user)) {
+            System.out.println(formattedDiscount = decimalFormat.format(-champagneDiscount.getValue()) + "원");
+        }
+        System.out.println(formattedDiscount);
+    }
     public static void printBenefit(User user) {
         DecimalFormat decimalFormat = new DecimalFormat("#,###");
         System.out.println("\n<혜택 내역>");
+        String formattedDiscount = "없음";
+        if (checkBenefit(user)) {
+            printDiscount("크리스마스 디데이 할인", Calculator.calculateChristmasDiscount(user.getVisitDate()), decimalFormat);
+            printDiscount("평일 할인", Calculator.calculateWeekdayDiscount(user.getVisitDate(), user.getOrderMap()), decimalFormat);
+            printDiscount("특별 할인", Calculator.calculateSpecialDiscount(user.getVisitDate()), decimalFormat);
+            printChampagneDiscount(Calculator.calculateChampagne(user), decimalFormat);
+        } else {
+            System.out.println(formattedDiscount);
+        }
 
-        printDiscount("크리스마스 디데이 할인", Calculator.calculateChristmasDiscount(user.getVisitDate()), decimalFormat);
-        printDiscount("평일 할인", Calculator.calculateWeekdayDiscount(user.getVisitDate(), user.getOrderMap()), decimalFormat);
-        printDiscount("특별 할인", Calculator.calculateSpecialDiscount(user.getVisitDate()), decimalFormat);
-        printChampagneDiscount(Calculator.calculateChampagne(user), decimalFormat);
 
         System.out.println("\n<총혜택 금액>");
         int totalDiscount = user.getTotalDiscount();
         int totalBenefit = totalDiscount + getChampagneDiscountValue(user);
         System.out.println(decimalFormat.format(-Math.max(totalBenefit, 0)) + "원");
     }
+
+    public static boolean checkBenefit(User user) {
+        return user.getTotalDiscount() > 0;
+    }
+
     private static void printDiscount(String label, int discount, DecimalFormat decimalFormat) {
         String formattedDiscount = "없음";
         if (discount == 0) {
