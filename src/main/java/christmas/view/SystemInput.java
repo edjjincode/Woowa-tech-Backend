609240fe9762e7aeef7fb15ab78c0d1e2262379a
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-14 15:41:11 +0900

    fix(Christmas): make sure error message shows up when the input is lower than 1 and bigger than  31

--- src/main/java/christmas/view/SystemInput.java
+++ src/main/java/christmas/view/SystemInput.java
@@ -3,6 +3,9 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 import christmas.validator.Validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SystemInput {
     public static Integer readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (1~30): ");
@@ -19,7 +22,7 @@ public class SystemInput {
     }
 
     public static void validateDate(int visitDate){
-        if (visitDate >= 1 && visitDate <= 30) {
+        if (visitDate < 1 && visitDate > 31) {
             throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
