e5fc3f0febe5439c3cc4db3cbe72cef52b6eb34c
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-14 13:35:03 +0900

    feat(Christmas): print out error message when input of the day is less than 1 or higher than 31
    - add try catch phrase in readDate so that it runs the method when wrong message returns

--- src/main/java/christmas/domain/DateUtil.java
+++ src/main/java/christmas/domain/DateUtil.java
@@ -3,16 +3,12 @@ package christmas.domain;
 import java.util.Calendar;
 public class DateUtil {
     public static boolean isWeekday(int visitDate) {
-        if (visitDate >= 1 && visitDate <= 30) {
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.YEAR, Calendar.getInstance().get(Calendar.YEAR));
-            calendar.set(Calendar.MONTH, Calendar.DECEMBER);
-            calendar.set(Calendar.DAY_OF_MONTH, visitDate);
-            int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-            return dayOfWeek != Calendar.FRIDAY && dayOfWeek != Calendar.SATURDAY;
-        } else {
-            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, Calendar.getInstance().get(Calendar.YEAR));
+        calendar.set(Calendar.MONTH, Calendar.DECEMBER);
+        calendar.set(Calendar.DAY_OF_MONTH, visitDate);
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        return dayOfWeek != Calendar.FRIDAY && dayOfWeek != Calendar.SATURDAY;
     }
 
     public static boolean isWeekend(int visitDate) {
--- src/main/java/christmas/view/SystemInput.java
+++ src/main/java/christmas/view/SystemInput.java
@@ -1,12 +1,27 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.validator.Validator;
 
 public class SystemInput {
     public static Integer readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (1~30): ");
         String date = Console.readLine();
-        return Integer.parseInt(date);
+        int visitDate = Integer.parseInt(date);
+        try {
+            validateDate(visitDate);
+        }
+        catch (IllegalArgumentException e) {
+            Validator.printErrorMessage(e.getMessage());
+            readDate();
+        }
+        return visitDate;
+    }
+
+    public static void validateDate(int visitDate){
+        if (visitDate >= 1 && visitDate <= 30) {
+            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
     }
 
     public static String readOrder() {
