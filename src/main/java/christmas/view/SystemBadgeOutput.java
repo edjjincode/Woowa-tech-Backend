fe07b7d0db9c2a73b9e75451208637b34a0dcc04
Jinchan Kim <138580800+edjjincode@users.noreply.github.com>
2023-11-15 09:43:42 +0900

    feat(Christmas): make badge enum class to print badge according to total benefit

--- src/main/java/christmas/view/SystemBadgeOutput.java
+++ src/main/java/christmas/view/SystemBadgeOutput.java
@@ -1,9 +1,7 @@
 package christmas.view;
 
-import christmas.controller.ChristmasController;
-import christmas.domain.Discount;
+import christmas.domain.Badge;
 import christmas.domain.User;
-
 import java.text.DecimalFormat;
 
 public class SystemBadgeOutput {
@@ -13,21 +11,18 @@ public class SystemBadgeOutput {
         System.out.println(decimalFormat.format(user.getFinalPayment()) + "원");
 
         System.out.println("\n<12월 이벤트 배지>");
-        if (user.getTotalDiscount() > 0) {
-            System.out.println(getBadgeByTotalDiscount(user.getTotalBenefit()));
-        } else {
-            System.out.println("없음");
-        }
+        System.out.println(Badge.getBadgeByTotalDiscount(user.getTotalBenefit()));
+
     }
-    private static String getBadgeByTotalDiscount(int totalBenefit) {
-        if (totalBenefit >= 20_000) {
-            return "산타";
-        } else if (totalBenefit >= 10_000) {
-            return "트리";
-        } else if (totalBenefit >= 5_000) {
-            return "별";
-        } else {
-            return "없음";
-        }
-    }
+//    private static String getBadgeByTotalDiscount(int totalBenefit) {
+//        if (totalBenefit >= 20_000) {
+//            return "산타";
+//        } else if (totalBenefit >= 10_000) {
+//            return "트리";
+//        } else if (totalBenefit >= 5_000) {
+//            return "별";
+//        } else {
+//            return "없음";
+//        }
+//    }
 }
